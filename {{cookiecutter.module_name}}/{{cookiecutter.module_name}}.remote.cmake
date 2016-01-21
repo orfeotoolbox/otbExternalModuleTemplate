9406abbe7db1e72ed2abedf13018c2c3c94a0126
Philippe Mallet-Ladeira <philippe@mallet-ladeira.name>
2016-01-21 23:01:30 +0100

    ENH: add module.remote.cmake template

---
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.remote.cmake
@@ -0,0 +1,7 @@
+# Contact: {{cookiecutter.full_name}} <{{cookiecutter.email}}>
+
+otb_fetch_module({{cookiecutter.module_name}}
+  "{{cookiecutter.module_short_description}}"
+  GIT_REPOSITORY {{cookiecutter.git_repository}}
+  GIT_TAG {{cookiecutter.git_tag}}
+  )
